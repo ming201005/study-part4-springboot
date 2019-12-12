ea179fee1cc96c4a3a008cb5c8e52b68eae4f231
ming201005 <272038088@qq.com>
2019-12-12 17:09:14 +0800

    提交2

---
+++ demo01/src/test/java/com/springboot/demo01/Demo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
